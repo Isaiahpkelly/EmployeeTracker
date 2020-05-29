9a8de982ec34ec14ec5852f4c4821dcab9a56190
Isaiah Kelly <isaiahpkelly@gmail.com>
2020-05-29 12:42:03 -0400

    retrying heroku

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const inquirer = require('inquirer');
 const addEmployee = require('./userRequest.js');
 const addtoDb = require('./addToDatabase');
 const viewDb = require('./viewDatabase');
+const express = require('express');
+const app = express()
+
+ 
+app.set('port', (process.env.PORT || 8080));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 
 var connection;
@@ -25,105 +38,6 @@ connection.connect(function (err) {
 });
 
 
-function createDepartment() {
-    console.log("Inserting a new department...\n");
-    var query = connection.query(
-      "INSERT INTO department SET ?",
-      {
-        name: "Rocky Road"
-        
-      }, function(err, res) {
-        if (err) throw err;
-        console.log(res.affectedRows + " department created!\n");
-        // Call updateProduct AFTER the INSERT completes
-        // updateProduct();
-      }
-    );
-  
-    // logs the actual query being run
-    console.log(query.sql);
-  }
 
-
-function updateProduct() {
-    console.log("Updating all Rocky Road quantities...\n");
-    var query = connection.query(
-        "UPDATE products SET ? WHERE ?",
-        [
-            {
-                quantity: 100
-            },
-            {
-                flavor: "Rocky Road"
-            }
-        ],
-        function (err, res) {
-            if (err) throw err;
-            console.log(res.affectedRows + " products updated!\n");
-            // Call deleteProduct AFTER the UPDATE completes
-            deleteProduct();
-        }
-    );
-
-    // logs the actual query being run
-    console.log(query.sql);
-}
-
-// function createProduct() {
-//     console.log("Inserting a new product...\n");
-//     var query = connection.query(
-//         "INSERT INTO products SET ?",
-//         {
-//             flavor: "Rocky Road",
-//             price: 3.0,
-//             quantity: 50
-//         },
-//         function (err, res) {
-//             if (err) throw err;
-//             console.log(res.affectedRows + " product inserted!\n");
-//             // Call updateProduct AFTER the INSERT completes
-//             updateProduct();
-//         }
-//     );
-
-//     // logs the actual query being run
-//     console.log(query.sql);
-// }
-
-
-        // function departmentName(){
-        //     inquirer.prompt([
-        //         {
-        //             type: "input",
-        //             message: "What would you like to name the new department?",
-        //             name: "deptName"
-                   
-        //         }
-        //     ])
-        //         .then(answer => {
-        //             console.log(answer.dept);
-        //             })
-                
-        //     }
-        
-        // function createdepartment() {
-        //     console.log("Inserting a new department...\n");
-        //     var query = connection.query(
-        //         "INSERT INTO department SET ?",
-        //         {
-        //             name: "Rocky Road",
-                    
-        //         },
-        //         function (err, res) {
-        //             if (err) throw err;
-        //             console.log(res.affectedRows + " product inserted!\n");
-        //             // Call updateProduct AFTER the INSERT completes
-        //             updateProduct();
-        //         }
-        //     );
-        
-        //     // logs the actual query being run
-        //     console.log(query.sql);
-        // }
 
         
